ff7468f4d04848fef70e587b53b739fba322aeb1
rgsax1 <rgsa@outlook.com.br>
2024-07-07 20:39:04 -0300

    Implementing security

---
+++ backend-spring/src/main/java/rodrigo/salles/todo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package rodrigo.salles.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rodrigo.salles.todo.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
